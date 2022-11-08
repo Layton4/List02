bd4efb8d019a9aba4661d6d1d1e79404e296ea7e
Layton4 <Javierete4774@gmail.com>
2022-11-08 20:22:01 +0100

    Exercice3 fet

--- Assets/Scripts/Exercici3_Casillas.cs
+++ Assets/Scripts/Exercici3_Casillas.cs
@@ -4,39 +4,72 @@ using UnityEngine;
 
 public class Exercici3_Casillas : MonoBehaviour
 {
-    private int movement = 150;
+    private int movement = 150; //The distance between each square on the board
+    private int boardLimit = 300;
+
+    private bool canMove = true; //boolean to detect if we can move in that moment
+    private float speed = 155f; //the speed the player will move from one square to the next
+    private Vector3 nextPosition; //the position the player is ordered to move next
+
+
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.RightArrow))
+        if(Input.GetKeyDown(KeyCode.RightArrow) && canMove)
         {
-            if(transform.position.x != 300)
+            if(transform.position.x != boardLimit) //if we want to move, we are not at the limit of the board and we can move
             {
-                gameObject.transform.position = new Vector3(transform.position.x + movement, transform.position.y, transform.position.z);
+                nextPosition = new Vector3(transform.position.x + movement, transform.position.y, transform.position.z);
             }
         }
 
-        else if(Input.GetKeyDown(KeyCode.LeftArrow))
+        else if(Input.GetKeyDown(KeyCode.LeftArrow) && canMove) //if we want to move, we are not at the limit of the board and we can move
         {
-            if (transform.position.x != -300)
+            if (transform.position.x != -boardLimit)
             {
-                gameObject.transform.position = new Vector3(transform.position.x - movement, transform.position.y, transform.position.z);
+                nextPosition = new Vector3(transform.position.x - movement, transform.position.y, transform.position.z);
             }
         }
 
-        else if(Input.GetKeyDown(KeyCode.UpArrow))
+        else if(Input.GetKeyDown(KeyCode.UpArrow) && canMove) //if we want to move, we are not at the limit of the board 
         {
-            if (transform.position.y != 300)
+            if (transform.position.y != boardLimit)
             {
-                gameObject.transform.position = new Vector3(transform.position.x, transform.position.y + movement, transform.position.z);
+                nextPosition = new Vector3(transform.position.x, transform.position.y + movement, transform.position.z);
             }
         }
 
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && canMove) //if we want to move, we are not at the limit of the board and we can move
         {
-            if (transform.position.y != -300)
+            if (transform.position.y != -boardLimit)
             {
-                gameObject.transform.position = new Vector3(transform.position.x, transform.position.y - movement, transform.position.z);
+                nextPosition = new Vector3(transform.position.x, transform.position.y - movement, transform.position.z);
             }
         }
+
+        if (nextPosition != transform.position) //if we ordered the player to move
+        {
+            //StartCoroutine(MovePlayer());
+            Moving();
+        }
+        if (nextPosition == transform.position)
+        {
+            canMove = true;
+            //StopAllCoroutines();
+        }
+    }
+
+    public IEnumerator MovePlayer()
+    {
+        canMove = false;
+
+        transform.position = Vector3.MoveTowards(transform.position, nextPosition, speed * Time.deltaTime);
+        yield return new WaitForSeconds(0f);
+    }
+
+    public void Moving()
+    {
+        canMove = false; //we avoid any other movement until we arrive to the next square
+
+        transform.position = Vector3.MoveTowards(transform.position, nextPosition, speed * Time.deltaTime); //our movement
     }
 }
